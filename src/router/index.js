bf0cc10a87d9b1ee129d0237926658336ef62cac
‘wsh’ <3112475244@qq.com>
2023-05-22 15:57:19 +0800

    下午

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,42 @@
+import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-/* Layout */
-import Layout from '@/views/layout'
-export const constantRoutes = [
-  {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/login', component: () => import('@/views/login') },
+    {
+      path: '/',
+      component: () => import('@/views/layout'),
+      redirect: '/home',
+      children: [
+        { path: 'home', component: () => import('@/views/home') },
+        { path: 'article', component: () => import('@/views/WorkOrder') }, // 运营工单
+        { path: 'maintenance', component: () => import('@/views/WorkOrder/modules/maintenance.vue') }, // 运维工单
+        { path: 'area', component: () => import('@/views/Pt/index.vue') }, // 区域管理
+        { path: 'point', component: () => import('@/views/Pt/modules/Point .vue') }, // 点位管理
+        { path: 'cooperation', component: () => import('@/views/Pt/modules/cooperation.vue') }, // 合作商管理
+        { path: 'equipment', component: () => import('@/views/equipment') }, // 设备管理
+        { path: 'status', component: () => import('@/views/equipment/modules/status.vue') }, // 设备状态
+        { path: 'type', component: () => import('@/views/equipment/modules/type.vue') }, // 设备类型管理
+        { path: 'people', component: () => import('@/views/people') }, // 人员列表
+        { path: 'statistics', component: () => import('@/views/people/modules/statistics.vue') }, // 人员统计
+        { path: 'WorkloadList', component: () => import('@/views/people/modules/WorkloadList.vue') }, // 工作量列表
+        { path: 'goodsType', component: () => import('@/views/goods') }, // 商品类型
+        { path: 'commodity', component: () => import('@/views/goods/modules/commodity.vue') }, // 商品管理
+        { path: 'strategic', component: () => import('@/views/strategic') }, // 策略管理
+        { path: 'order', component: () => import('@/views/order') }, // 订单管理
+        { path: 'reconciliation', component: () => import('@/views/Reconciliation') } // 对账统计
 
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
-
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '首页', icon: 'dashboard' }
-    }]
-  },
-
-  // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
-
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  routes: constantRoutes
+      ]
+    }
+  ]
 })
 
-const router = createRouter()
-
-export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
-}
-
+// router.beforeEach((to, from, next) => {
+//   if (!store.state.user.token && to.path !== '/login') return next('/login')
+//   next()
+// })
 export default router
